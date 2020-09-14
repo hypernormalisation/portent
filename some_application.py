c71abe2df13673836e5e1cf0781af95511c5fdc2
Stephen Ogilvy <hypernormalisation@tuta.io>
2020-09-14 04:04:42 +0100

    cleanup

--- some_application.py
+++
@@ -1,9 +0,0 @@
-__name__ = 'tradingsolutions'
-__version__ = '1.2.2'
-
-import portent
-
-portent.welcome(name=__name__, version=__version__, theme='basic')
-
-# Test it only runs once per session.
-portent.welcome()
